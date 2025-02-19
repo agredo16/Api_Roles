120b9bd9037ff5556a6180c04d49d6aad21eb75a
Yudith Agredo <agredocatalina90@gmail.com>
2025-02-18 21:39:51 -0500

    cambio linea 7

--- app/config/dbClient.js
+++ app/config/dbClient.js
@@ -4,8 +4,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const uri = process.env.MONGO_URI; 
-const client = new MongoClient(uri);
-
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const dbClient = {
     db: null,
     async connect() {
